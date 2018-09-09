5927fa1a80ae71130e490652800c599109e80144
Lachlan Blake <lachlan.blake10@gmail.com>
2018-09-09 13:35:38 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const fs = require('fs');
 const https = require('https');
+
+https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (response) => {
+	var body = '';
+	
+	response.on('data', (data) => {
+		body += data;
+	});
+	
+	response.on('end', () => {
+		JSON.parse(body);
+	});
+});
