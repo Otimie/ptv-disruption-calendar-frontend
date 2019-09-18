f8cb13e1aea561b228c6217c9b682080f403420e
Lachlan Blake <lachlan.blake10@gmail.com>
2019-09-18 19:08:16 +1000

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+ReactDOM.render(
+	<div class="card my-3">
+		<div class="card-header">Train</div>
+		<div class="list-group list-group-flush">
+			<a class="list-group-item list-group-item-action" href="https://ptv-disruption-calendar.lachlanb.me/1">Alamein</a>
+		</div>
+	</div>,
+	document.getElementById('routes')
+);
