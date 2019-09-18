234fab1b4d774b8287cd3d3ba9f29ebd0b1fab6c
Lachlan Blake <lachlan.blake10@gmail.com>
2019-09-18 20:04:59 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
-function App() {
+function Route() {
+	return (
+		<a class="list-group-item list-group-item-action" href="https://ptv-disruption-calendar.lachlanb.me/1">Alamein</a>
+	);
+}
+
+function Routes() {
+	return (
+		<Route />
+	);
+}
+
+function RouteType() {
 	return (
 		<div class="card my-3">
 			<div class="card-header">Train</div>
 			<div class="list-group list-group-flush">
-				<a class="list-group-item list-group-item-action" href="https://ptv-disruption-calendar.lachlanb.me/1">Alamein</a>
+				<Routes />
 			</div>
 		</div>
+	);
+}
+
+function RouteTypes() {
+	return (
+		<RouteType />
+	);
+}
+
+function App() {
+	return (
+		<RouteTypes />
 	);
 }
 
